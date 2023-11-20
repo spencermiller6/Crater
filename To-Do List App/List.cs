5468e121c4e03bcba7756cf7c51d7a3ebfa206aa
Spencer Miller <spencer.miller6.0@gmail.com>
2023-11-19 19:54:18 -0800

    Created constructors

--- To-Do List App/List.cs
+++ To-Do List App/List.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Windows.Documents;
+using static System.Collections.Specialized.BitVector32;
 
 namespace To_Do_List_App
 {
@@ -16,12 +17,25 @@ namespace To_Do_List_App
         string Name;
         Dictionary<string, string> Properties;
         List<Section> Sections;
+
+        public List(string name)
+        {
+            Name = name;
+            Properties = new Dictionary<string, string>();
+            Sections = new List<Section>();
+        }
     }
 
     public class Section
     {
         public string Name;
         public List<Item> Items;
+
+        public Section(string name)
+        {
+            Name = name;
+            Items = new List<Item>();
+        }
     }
 
     public class Item
@@ -29,5 +43,12 @@ namespace To_Do_List_App
         public string Name;
         public Dictionary<string, string> Properties;
         public List<Item> Children;
+
+        public Item(string name)
+        {
+            Name = name;
+            Properties = new Dictionary<string, string>();
+            Children = new List<Item>();
+        }
     }
 }
