4644fd7634c59d6a8734bc9560a643698e483d34
Spencer Miller <spencer.miller6.0@gmail.com>
2023-12-01 19:12:06 -0800

    Created static class to store general list information

---
+++ To-Do List App/MasterList.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using To_Do_List_App.Properties.ListProperties;
+
+namespace To_Do_List_App
+{
+    public static class MasterList
+    {
+        public static List<ListProperty> ListProperties { get; private set; }
+
+        static MasterList()
+        {
+            ListProperties = new List<ListProperty>
+            {
+                new CompletionProperty()
+            };
+        }
+    }
+}
