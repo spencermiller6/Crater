5197ddbc644820872b3db6f0bc98baad474ad1b7
Spencer Miller <spencer.miller6.0@gmail.com>
2023-12-01 18:42:08 -0800

    Created ListProperty class

---
+++ To-Do List App/ListProperty.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_Do_List_App
+{
+    public abstract class ListProperty
+    {
+        public List<string> AllowedValues { get; protected set; }
+
+        public ListProperty()
+        {
+            AllowedValues = new List<string>();
+            BuildValues();
+        }
+
+        protected abstract void BuildValues();
+    }
+
+    public class CompletionProperty : ListProperty
+    {
+        protected override void BuildValues()
+        {
+            AllowedValues.Add("Immediate");
+            AllowedValues.Add("Long-Term");
+            AllowedValues.Add("Disabled");
+        }
+    }
+}
